2334a681a4e6e5560f36361c75c047b819c8d06e
HeYakang <yushuiFFF@163.com>
2021-07-12 09:56:12 +0800

    fix_filenane_format

--- dao/db/orm.go
+++ dao/db/orm.go
@@ -8,16 +8,16 @@ import (
 
 func NewOrm() *gorm.DB{
 	//配置MySQL连接参数
-	username := "root"  //账号
+	userName := "root"  //账号
 	password := "123"   //密码
 	host := "127.0.0.1" //数据库地址，可以是Ip或者域名
 	port := 3306        //数据库端口
-	Dbname := "test_db" //数据库名
+	DBName := "test_db" //数据库名
 
 	//通过前面的数据库参数，拼接MYSQL DSN， 其实就是数据库连接串（数据源名称）
 	//MYSQL dsn格式： {username}:{password}@tcp({host}:{port})/{Dbname}?charset=utf8&parseTime=True&loc=Local
 	//类似{username}使用花括号包着的名字都是需要替换的参数
-	dsn := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	dsn := fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", userName, password, host, port, DBName)
 	//db,err := gorm.Open("mysql","root:123@(127.0.0.1)/test?charset=utf8mb4&loc=Local")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		//一些配置
